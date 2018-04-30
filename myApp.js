316743b17c955088636cb68c7cac27301951e0ef
Ateeq Ahmed <ahmedateeq64@gmail.com>
2018-05-01 02:33:52 +0530

    Add gpio-rpi and required logic

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
-var gpio = require('gpio');
+var gpio = require('rpi-gpio');
 var accP1 = 23;
 var accP2 = 24;
 var steerP1 = 17;
 var steerP2 = 27;
+
+gpio.setMode('mode_bcm');
 
 module.exports = {
 
@@ -41,25 +43,73 @@ module.exports = {
 
     fwd : function(){
         console.log('Forward!!');
+
+        gpio.write(accP1, true, function(err) {
+            if (err) throw err;
+            console.log('Written to pin');
+        });
+        gpio.write(accP2, false, function(err) {
+            if (err) throw err;
+            console.log('Written to pin');
+        });
+
+        
     },
 
     revr : function () {
         console.log('Reverse!!');
+
+        gpio.write(accP1, false, function(err) {
+            if (err) throw err;
+            console.log('Written to pin');
+        });
+        gpio.write(accP2, true, function(err) {
+            if (err) throw err;
+            console.log('Written to pin');
+        });
     },
 
     left : function () {
         console.log('Left!!');
+
+        gpio.write(steerP1, true, function(err) {
+            if (err) throw err;
+            console.log('Written to pin');
+        });
+        gpio.write(steerP2, false, function(err) {
+            if (err) throw err;
+            console.log('Written to pin');
+        });
     },
 
     right : function () {
         console.log('Right!!');
+
+        gpio.write(steerP1, false, function(err) {
+            if (err) throw err;
+            console.log('Written to pin');
+        });
+        gpio.write(steerP2, true, function(err) {
+            if (err) throw err;
+            console.log('Written to pin');
+        });
     },
 
     start : function () {
         console.log('Start!!');
+
+        gpio.setup(accP1, gpio.DIR_OUT);
+        gpio.setup(accP2, gpio.DIR_OUT);
+        gpio.setup(steerP1, gpio.DIR_OUT);
+        gpio.setup(steerP2, gpio.DIR_OUT);
+
     },
 
     end : function () {
         console.log('End!!');
+
+        gpio.destroy(function() {
+            console.log('All pins unexported');
+        });
     }
 };
